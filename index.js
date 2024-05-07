a0554d354dd8f12a6bf9173709d5227cd56f020c
tungbeo3452 <girl.keelypun0suy@hotmail.com>
2024-05-07 22:04:54 +0700

    aptos

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function findMaxLength(nums) {
-  const map = new Map();
-  map.set(0, -1);
-  let count = 0;
-  let maxLength = 0;
+function moveZeroes(nums) {
+  let index = 0;
   for (let i = 0; i < nums.length; i++) {
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-      maxLength = Math.max(maxLength, i - map.get(count));
-    } else {
-      map.set(count, i);
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
     }
   }
-  return maxLength;
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
 }
