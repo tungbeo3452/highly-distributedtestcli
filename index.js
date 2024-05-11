3db41066dbd74b40ba7bfe98f89f064d95af1020
tungbeo3452 <girl.keelypun0suy@hotmail.com>
2024-05-11 14:38:43 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
+const colors = ["red", "green", "blue"];
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+console.log(randomColor);
