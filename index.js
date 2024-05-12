31ab168de817c405c1a3bf468e8ce381af19f154
tungbeo3452 <girl.keelypun0suy@hotmail.com>
2024-05-12 11:36:46 +0700

    hello

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+function allPathsSourceTarget(graph) {
+  const result = [];
+  const target = graph.length - 1;
+  dfs(graph, 0, [0]);
+  function dfs(graph, node, path) {
+    if (node === target) {
+      result.push([...path]);
+      return;
+    }
+    for (const neighbor of graph[node]) {
+      path.push(neighbor);
+      dfs(graph, neighbor, path);
+      path.pop();
+    }
+  }
+  return result;
+}
